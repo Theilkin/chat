ae0f26094d37b6b0afa555763f95af382aab8b73
Ferid Abusov <122831032+Theferid@users.noreply.github.com>
2024-02-27 02:55:07 +0400

    Update read.py

--- Venom/modules/helpers/read.py
+++ Venom/modules/helpers/read.py
@@ -6,45 +6,41 @@ START = f"""
 **Qruplar üçün yaradılmış Çat Botuyam.**
 **──────────────**
 **➻ İstifadəsi /chatbot [on/off]**
-<b>||๏ Daha Ətraflı məlumat üçün Kömək Buttonuna Toxununun.||</b>
+<b>||๏ @The_ferid hazırlamışdır !||</b>
 """
 
 HELP_READ = f"""
-<u>**{VenomX.name}- üçün əmr**</u>
-<u>**aşağıda verilmişdir!**</u>
-**BÜTÜN ƏMR-LƏRİ / İL İSTİFADƏ OLUNA BİLƏR:/**
+<u>**{VenomX.name}- üçün**</u>
+<u>**Əmrlər aşağıda verilmişdir!**</u>
+**Bütün əmrləri / ilə istifadə edə bilərsiniz**
 **──────────────**
 <b>||©️ @{OWNER_USERNAME}||</b>
 """
 
 TOOLS_DATA_READ = f"""
 <u>**{VenomX.name}- üçün alətlər:**</u>
-**➻ QAYNAQ /repo ÜÇÜN MƏNBƏ KODUNU ALMAQ!**
+**➻ Kommand /ping {VenomX.name}- un pingini yoxlayın**
 **──────────────**
-**➻ QAYNAQ /ping ÜÇÜN {VenomX.name}- in PİNGİNİ YOXLAYIN**
-**──────────────**
-**➻ ÖZ İSTİFADƏÇİ İD, SÖHBƏT İD VƏ MESAJ İD-İNİ ƏLDƏ ETMƏK ÜÇÜN /id-İ İSTİFADƏ EDİN.**
+**➻ öz istifadəçi id, söhbət id və öz id - ni əldə etmək üçün /id komutu ilə istifadə edin.**
 **──────────────**
 <b>||©️ @{OWNER_USERNAME}||</b>
 """
 
 CHATBOT_READ = f"""
 <u>**{VenomX.name}- üçün əmr**</u>
-**➻ /chatbot İSTİFADƏ ETMƏK ÜÇÜN CHATBOTU AKTİV ETMƏK/QEYD ETMƏK**
-**๏ Qeyd ➻ YUXARIDAKI ƏMR YALNIZCA QRUPDA İŞLƏYİR!!**
-**───────────────**
+**➻ /chatbot (on/off) ilə aktiv edə bilərsiniz**
+**๏ Qeyd yuxarıdakə əmr sadəcə qruplar üçün etibarlıdır!!**
 <b>||©️ @{OWNER_USERNAME}||</b>
 """
 
 SOURCE_READ = f"**Salam, [{VenomX.name}](https://t.me/{VenomX.username})- in mənbə kodu aşağıda verilmişdir.**\n**XAHIŞ EDİRƏM REPO-Nİ FORK EDİN VƏ YILDIZI VERİN ✯**\n**──────────────────**\n**MƏNBƏ KODU [burada](https://github.com/venombolteop/ChatbotV2)**\n**──────────────────**\n**ƏGƏR HANSI-SA PROBLEMİ BAŞ VERƏRSƏ, ONDAN SONRA [DƏSTƏK QURULUŞUNA](https://t.me/{SUPPORT_GRP}) MÜRACİƏT EDİN.\n<b>||©️ @{OWNER_USERNAME}||</b>"
 
-ADMIN_READ = f"Təzədir"
+ADMIN_READ = f"Yenidir"
 
 ABOUT_READ = f"""
-**➻ [{VenomX.name}](https://t.me/{VenomX.username})- in bir AI əsaslı çat-botdur.**
+**➻ [{VenomX.name}](https://t.me/{VenomX.username})- in bir çat-botdur.**
 **➻ [{VenomX.name}](https://t.me/{VenomX.username})- avtomatik olaraq istifadəçiyə cavab verir.**
 **➻ Qruplarınızı aktivləşdirməyinizə kömək edir.**
 **➻ [Python](https://www.python.org) ilə yazılıb [MongoDB](https://www.mongodb.com) kimi verilənlər bazası ilə.**
-**──────────────**
 **➻ [{VenomX.name}](https://t.me/{VenomX.username})- haqqında əsas kömək və məlumat üçün aşağıda verilmiş düymələrə basın**
 """
